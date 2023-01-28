210b3333b1144987c6bbfad7a5d1362fa247bbf5
sudarshan441 <sudarshanpujari812@gmail.com>
2023-01-28 14:17:32 +0530

    added

--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 app.get("/",async(req,res)=>{
-    const {category,level,total} = req.query
-    const data =await quizModel.find({difficulty:"easy"}).limit(total);
+    const {category,level,page} = req.query
+    const data =await quizModel.find({category:category,difficulty:level}).skip(page-1).limit(1);
  res.send(data)
 })
+// app.get("/questions",async(req,res)=>{
+
+//     const {category,level,total} = req.query
+//     const data =await quizModel.find({category:category,difficulty:level}).limit(total);
+//  res.send(data)
+// })
 
 mongoose.connect("mongodb+srv://sudarshan:sudarshan@cluster0.ydgncyl.mongodb.net/mock15").then(()=>{
     app.listen(8005,()=>{
